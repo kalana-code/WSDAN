c6aa01dd9ba788124685e616cbcd7fcbb5c2b4ea
Malinda Wimalasena <malindawimalasena@gmail.com>
2020-06-15 17:04:22 +0530

    Packet analyzer of the controller is implemented

--- controller/backend/src/Beq/packethandler/utils/utils.go
+++ controller/backend/src/Beq/packethandler/utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"log"
 	"net"
+	"os/exec"
 	"strings"
 )
 
@@ -32,4 +33,43 @@ func GetIPAndMAC() (string, string, error) {
 		return "nil", "nil", err
 	}
 	return ipAddr, hwAddr.String(), nil
+}
+
+// IptableInitializer is used to intialize the iptables of the node
+func IptableInitializer() error {
+	log.Println(infoLog, "Run IptableInitializer")
+	err := iptableRuleFlusher()
+	if err != nil {
+		log.Println(errorLog, "Error when flushing iptables")
+		return err
+	}
+	err = iptableInputHandler()
+	if err != nil {
+		log.Println(errorLog, "Error when handling INPUT in iptables")
+		return err
+	}
+	return err
+}
+
+// iptableRuleFlusher is used to flush iptable rules
+func iptableRuleFlusher() error {
+	log.Println(infoLog, "Flushing iptable rules")
+	return exec.Command("sudo", "iptables", "-F").Run()
+}
+
+// iptableInputHandler is used to insert iptable INPUT rules
+func iptableInputHandler() error {
+	log.Println(infoLog, "Inserting iptable INPUT rules")
+	ip, _, _ := GetIPAndMAC()
+	err := exec.Command("sudo", "iptables", "-A", "INPUT", "1", "-p", "tcp", "--dport", "22", "-j", "ACCEPT").Run()
+	if err != nil {
+		log.Println(errorLog, "Error when inserting INPUT rule: ssh")
+		return err
+	}
+	err = exec.Command("sudo", "iptables", "-A", "INPUT", "2", "-d", ip, "-j", "ACCEPT").Run()
+	if err != nil {
+		log.Println(errorLog, "Error when inserting INPUT rule: allow packet with dst ip as node ip")
+		return err
+	}
+	return exec.Command("sudo", "iptables", "-A", "INPUT", "3", "-i", "wlan0", "-j", "NFQUEUE", "--queue-num", "0").Run()
 }
