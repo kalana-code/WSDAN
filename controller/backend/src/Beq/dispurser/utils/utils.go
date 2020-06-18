8ea06e8f20a23fb52276cb530734cfff8ae2943f
Kalana Dhanajaya <kalanad@eng.pdn.ac.lk>
2020-06-18 09:40:09 +0530

    [dispurser] fixed issue

--- controller/backend/src/Beq/dispurser/utils/utils.go
+++ controller/backend/src/Beq/dispurser/utils/utils.go
@@ -5,7 +5,9 @@ package utils
 import (
 	"Beq/dispurser/model"
 	"bytes"
+	"encoding/json"
 	"log"
+	"net"
 	"net/http"
 )
 
@@ -32,11 +34,11 @@ func Dispurse(job *model.Job) {
 
 func removeRule(job *model.Job) {
 	// figureout host IP
-	// host := net.ParseIP(job.NodeIP)
-	// if host == nil {
-	// 	log.Println("ERROR: [RD]: [Remove Rule] IP convertion process was failed")
-	// 	return
-	// }
+	host := net.ParseIP(job.NodeIP)
+	if host == nil {
+		log.Println("ERROR: [RD]: [Remove Rule] IP convertion process was failed")
+		return
+	}
 
 	//figureout remove rule model
 	data, ok := job.TaskDetails.(model.RemoveRuleJob)
@@ -44,45 +46,74 @@ func removeRule(job *model.Job) {
 		log.Println("ERROR: [RD]: [Remove Rule] Task details not match to RemoveRuleJob model")
 		return
 	}
-	httpClient(job.NodeIP, model.RemoveRuleEndPoint, data)
+	httpClient(host, model.RemoveRuleEndPoint, data)
 
 }
 
 func addRule(job *model.Job) {
 	// figureout host IP
-	// host := net.ParseIP(job.NodeIP)
-	// if host == nil {
-	// 	log.Println("ERROR: [RD]: [Remove Rule] IP convertion process was failed")
-	// 	return
-	// }
+	host := net.ParseIP(job.NodeIP)
+	if host == nil {
+		log.Println("ERROR: [RD]: [Remove Rule] IP convertion process was failed")
+		return
+	}
 
-	//figureout remove rule model
-	// data, ok := job.TaskDetails.(model.AddRuleJob)
-	// if !ok {
-	// 	log.Println("ERROR: [RD]: [Add Rule] Task details not match to AddRuleJob model")
-	// 	return
-	// }
-	httpClient(job.NodeIP, model.AddRuleEndPoint, job.TaskDetails)
+	// figureout remove rule model
+	data, ok := job.TaskDetails.(model.AddRuleJob)
+	if !ok {
+		log.Println("ERROR: [RD]: [Add Rule] Task details not match to AddRuleJob model")
+		return
+	}
+	httpClient(host, model.AddRuleEndPoint, data)
 }
 
-func httpClient(host string, endPoint string, data interface{}) {
+func httpClient(host net.IP, endPoint string, data interface{}) {
 
-	url := host + ":" + model.Port + "/" + endPoint
-	jsonData, ok := data.([]byte)
-	if !ok {
-		log.Println("ERROR: [HC]: [HTTP Client] Request data convertion [to []byte ] process was failed")
-		return
-	}
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
-	req.Header.Set("X-Custom-Header", "myvalue")
-	req.Header.Set("Content-Type", "application/json")
+	url := "http://" + host.String() + ":" + model.Port + "/" + endPoint
+	AddRule, isAddRule := data.(model.AddRuleJob)
+	RemoveJob, isRemoveJob := data.(model.RemoveRuleJob)
+	if isAddRule {
+		jsonData := map[string]interface{}{
+			"RuleID":    AddRule.RuleID,
+			"Protocol":  AddRule.Protocol,
+			"FlowID":    AddRule.FlowID,
+			"DstIP":     AddRule.DstIP,
+			"Interface": AddRule.Interface,
+			"DstMAC":    AddRule.DstMAC,
+			"Action":    AddRule.Action,
+		}
+		jsonValue, _ := json.Marshal(jsonData)
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonValue))
+		req.Header.Set("X-Custom-Header", "myvalue")
+		req.Header.Set("Content-Type", "application/json")
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Println("ERROR: [HC]: [HTTP Client] Request failed.")
-		return
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Println("ERROR: [HC]: [HTTP Client] Request failed.")
+			return
+		}
+		defer resp.Body.Close()
+		log.Println("INFO: [HC]: [HTTP Client] Status Request.", resp.Status)
+	} else if isRemoveJob {
+		jsonData := map[string]interface{}{
+			"RuleID": RemoveJob.RuleID,
+		}
+		jsonValue, _ := json.Marshal(jsonData)
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonValue))
+		req.Header.Set("X-Custom-Header", "myvalue")
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Println("ERROR: [HC]: [HTTP Client] Request failed.")
+			return
+		}
+		defer resp.Body.Close()
+		log.Println("INFO: [HC]: [HTTP Client] Status Request.", resp.Status)
+	} else {
+		log.Println("INFO: [HC]: Invalid JOB data")
 	}
-	defer resp.Body.Close()
-	log.Println("INFO: [HC]: [HTTP Client] Status Request.", resp.Status)
+
 }
